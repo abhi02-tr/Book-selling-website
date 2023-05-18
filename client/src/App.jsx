4ade8e1b994bbf0431eca3ce026a258a0082ca9e
Abhi Trivedi <abhi02.trivedi@gmail.com>
2023-05-18 11:47:32 +0530

    Navigation is added and basic component building and styling using tailwind css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
+import {Routes, Route, Link} from "react-router-dom";
 import './App.css'
 
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import PageNotFound from './pages/PageNotFound';
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='text-3xl font-bold text-center p-5 bg-slate-700 text-white'>
+        Book-selling-website
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+      {/* Navbar */}
+      <nav className='bg-gray-200 dark:bg-gray-700'>
+        <div className='max-w-screen-xl px-4 py-3 mx-auto'>
+          <div className='flex items-center'>
+            <ul className='flex space-x-8 font-medium mt-0 text-m mr-4'>
+              <li>
+                <Link to='/' className='text-gray-800 hover:underline'>Home</Link>
+              </li>
+              <li>
+                <Link to='/login' className='text-gray-800 hover:underline'>Login</Link>
+              </li>
+              <li>
+                <Link to='/signup' className='text-gray-800 hover:underline'>Signup</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
     </>
   )
 }
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,11 @@
+const Login = () => {
+    return (
+        <>
+            <div className="text-center m-auto text-6xl">
+                Login
+            </div>
+        </>
+    );
+};
+
+export default Login
